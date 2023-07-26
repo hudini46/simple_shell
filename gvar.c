cc1652a9f5291258291ff31ef2b1b64d2f68fdba
Promestromae <nzuvameshack@gmail.com>
2023-07-26 14:59:24 -0700

    lovely moments

--- gvar.c
+++ gvar.c
@@ -1,9 +1,9 @@
 #include "gvar.h"
 
-/* Definition of program_name variable */
+/* Declaring of program_name variable */
 char *program_name;
 
-/* Definition of buffer variable */
+/* Declaring of buffer variable */
 char *buffer = NULL;
 size_t bufsize = 0;
 ssize_t nread;
